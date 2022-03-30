1be5f38d5db433848e71c7b7a7c36a206c073445
glubdub <gabrielferreira5247@gmail.com>
2022-03-30 10:44:59 -0300

    Fixing markdown render bug

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -10,7 +10,7 @@ const Post: NextPage<PostType> = (post) => {
       <div className="11/12 lg:w-7/12 flex justify-center flex-col bg-[#42414d] border border-solid border-transparent shadow-sm rounded-lg p-5">
         <Image alt="Post cover image" src={post.img} width={700} height={400}></Image>
         <h1 className="text-3xl mt-10">{post.title}</h1>
-        <p className="text-lg leading-8 mt-6" dangerouslySetInnerHTML={{ __html: post.content }}></p>
+        <div className="text-lg leading-8 mt-6" dangerouslySetInnerHTML={{ __html: post.content }}></div>
         <div className="flex justify-end">
           <p className="text-base text-gray-400 mt-6">{post.date}</p>
         </div>
